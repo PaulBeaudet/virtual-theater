bf9d12c32d2aa1d286cccb15ce12ba33ef5c56ac
Paul Beaudet <inof8or@gmail.com>
2020-09-10 08:11:26 -0400

    remove redux, change when websocket opens to make sure the room is only open if authorized, add some discriptions

--- src/components/Theater.tsx
+++ src/components/Theater.tsx
@@ -15,49 +15,57 @@ const Theater: React.FC = () => {
   const history = useHistory()
   const { state, dispatch } = useContext(GlobalUserContext)
   useEffect(() => {
+    // signal server sends to update one user
     ws.on('new_user', (req: any) => {
       dispatch({
         type: 'ADD_USER',
         payload: req,
       })
     })
+    // Signal server sends to completely refresh meeple positions
     ws.on('load_room', (req: any) => {
       dispatch({
         type: 'LOAD_ROOM',
         payload: req.roomLayout,
       })
     })
+    // Signal server sends when all spots are taken
     ws.on('room full', () => {
       alert('Room full! Sorry')
     })
+    // signal server sends when a table is taken
     ws.on('table_taken', () => {
       alert('Table taken! Sorry')
     })
-    ws.init(() => {
-      Firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-          const addUserData: personalType = {
-            displayName: user.displayName,
-            photoURL: user.photoURL,
-            uid: user.uid,
-          }
-          dispatch({
-            type: 'SIGN_IN',
-            payload: addUserData
-          })
-          ws.msg(`new-user`, addUserData)
-          window.addEventListener('beforeunload', () => {
-            ws.msg('unload', {
-              uid: user.uid
-            })
-          })
-        } else {
-          history.push('/')
+    Firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        const addUserData: personalType = {
+          displayName: user.displayName,
+          photoURL: user.photoURL,
+          uid: user.uid,
         }
-      })
+        // Populate local user logged in info
+        dispatch({
+          type: 'SIGN_IN',
+          payload: addUserData
+        })
+        // make connection to WebSocket server
+        ws.init(() => {
+          ws.msg(`new-user`, addUserData)
+        })
+        // Provide a signal for server to know when user disconnects
+        window.addEventListener('beforeunload', () => {
+          ws.msg('unload', {
+            uid: user.uid
+          })
+        })
+      } else { // given no user show auth route
+        history.push('/')
+      }
     })
   }, [dispatch, history])
 
+  // only show this component if logged in
   if (state.loggedIn === null) { return null }
   return (
     <div className='remo-theater' style={{
