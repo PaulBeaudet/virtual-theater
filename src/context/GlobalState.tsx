3e64a668262401552aeb5f0ae3d871d00d508ba4
Paul Beaudet <inof8or@gmail.com>
2020-09-14 13:41:07 -0400

    add license

--- src/context/GlobalState.tsx
+++ src/context/GlobalState.tsx
@@ -1,3 +1,18 @@
+// GlobalState.tsx Copyright 2020 Paul Beaudet
+// This file is part of Virtual-Theater.
+
+// Virtual-Theater is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Virtual-Theater is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Virtual-Theater.  If not, see <https://www.gnu.org/licenses/>.
 import React, { createContext, useReducer } from 'react'
 import AppReducer from './AppReducer'
 import { GlobalContextType, userType, personalType } from '../interfaces'
